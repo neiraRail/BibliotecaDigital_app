8a9ad916bf8c8d53994c4790d091a66aa295b426
Rail Neira <neira.rail@gmail.com>
2022-05-14 22:51:31 -0400

    Crea e implementa vista de datos de un libro a nivel de mockup, sin funcionalidad

---
+++ lib/datos_libros.dart
@@ -0,0 +1,72 @@
+import 'package:bib_digitalapp/base_app_bar.dart';
+import 'package:flutter/material.dart';
+
+class VistaDatosLibros extends StatefulWidget {
+  const VistaDatosLibros({Key? key}) : super(key: key);
+
+  @override
+  State<VistaDatosLibros> createState() => _VistaDatosLibrosState();
+}
+
+class _VistaDatosLibrosState extends State<VistaDatosLibros> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: BaseAppBar(
+          title: const Text("Datos Libro"),
+          appBar: AppBar(),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+          child: Container(
+            decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(15),
+                ),
+                color: Colors.grey),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Stack(
+                children: [
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          Image.network(
+                            'https://picsum.photos/250?image=9',
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          const Text(
+                            "Titulo Del Libro",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Center(
+                          child: Text(
+                              "Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción")),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(primary: Colors.green),
+                        onPressed: () {},
+                        child: const Text("RESERVA"),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
