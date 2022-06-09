652960819df570e18ff893f7a86dd5220fb07afc
Rail Neira <neira.rail@gmail.com>
2022-06-08 23:59:20 -0400

    Agrega funcionalidades de busqueda a vista web

--- lib/web/buscador.dart
+++ lib/web/buscador.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+
 import 'package:bib_digitalapp/libro_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:bib_digitalapp/base_app_bar.dart';
 import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
 
 import '../modelo/libro.dart';
 
@@ -15,7 +18,8 @@ class BuscadorWeb extends StatefulWidget {
 }
 
 class _BuscadorWebState extends State<BuscadorWeb> {
-  List<Map> busqueda = [];
+  List<Libro> busqueda = [];
+  final busquedaController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,10 +30,16 @@ class _BuscadorWebState extends State<BuscadorWeb> {
         child: Column(children: [
           Row(
             children: [
-              const Expanded(child: TextField()),
+              Expanded(
+                  child: TextField(
+                controller: busquedaController,
+              )),
               IconButton(
                 color: Colors.blueAccent,
-                onPressed: () {},
+                onPressed: () async {
+                  busqueda = await buscarPalabra(busquedaController.text);
+                  setState(() {});
+                },
                 icon: const Icon(Icons.search),
               )
             ],
@@ -43,28 +53,44 @@ class _BuscadorWebState extends State<BuscadorWeb> {
             ],
           ),
           Expanded(
-              child: Container(
-                  decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(15),
-                      ),
-                      color: Colors.deepPurple),
-                  child: GridView.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        childAspectRatio: 7 / 2,
-                      ),
-                      itemCount: busqueda.length,
-                      itemBuilder: (context, i) {
-                        return  GestureDetector(
-                          onTap: () =>Navigator.pushNamed(context, 'datos'),
-                          child: LibroCard(libro: Libro())
-                    
-                        );
-                      })))
+            child: Container(
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                  color: Colors.deepPurple),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 7 / 2,
+                ),
+                itemCount: busqueda.length,
+                itemBuilder: (context, i) {
+                  return GestureDetector(
+                      onTap: () => Navigator.pushNamed(context, 'datos',
+                          arguments: busqueda[i]),
+                      child: LibroCard(libro: busqueda[i]));
+                },
+              ),
+            ),
+          )
         ]),
       ),
     );
+  }
+
+  Future<List<Libro>> buscarPalabra(palabra) async {
+    final response = await http.get(
+        Uri.http("200.13.5.14:7102", "/busqueda/" + palabra, {'q': '{http}'}));
+
+    if (response.statusCode == 200) {
+      Iterable l = json.decode(response.body);
+      List<Libro> libros =
+          List<Libro>.from(l.map((model) => Libro.fromJson(model)));
+
+      return libros;
+    } else {
+      return [];
+    }
   }
 }
