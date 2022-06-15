c1065132a8cb432a4cd8fe6f2cf90fff0e9b6b15
Rail Neira <neira.rail@gmail.com>
2022-06-15 11:53:47 -0400

    Vista historial realiza petición http al inicializar

--- lib/historial.dart
+++ lib/historial.dart
@@ -1,7 +1,10 @@
 import 'package:bib_digitalapp/base_app_bar.dart';
 import 'package:bib_digitalapp/libro_card.dart';
 import 'package:bib_digitalapp/modelo/libro.dart';
+import 'package:bib_digitalapp/services/prestamoService.dart';
 import 'package:flutter/material.dart';
+
+import 'modelo/prestamo.dart';
 
 class VistaHistorial extends StatefulWidget {
   const VistaHistorial({Key? key}) : super(key: key);
@@ -11,6 +14,25 @@ class VistaHistorial extends StatefulWidget {
 }
 
 class _VistaHistorialState extends State<VistaHistorial> {
+  bool isLoaded = false;
+  List<Prestamo>? prestamos;
+
+  @override
+  void initState() {
+    super.initState();
+
+    fetchPrestamos();
+  }
+
+  fetchPrestamos() async {
+    prestamos = await PrestamoService.getPrestamosPorAlumno(1);
+    if (prestamos != null) {
+      setState(() {
+        isLoaded = true;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,80 +40,86 @@ class _VistaHistorialState extends State<VistaHistorial> {
         title: const Text("Historial"),
         appBar: AppBar(),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 30),
-        child: Container(
-          decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(
-                Radius.circular(15),
+      body: Visibility(
+        visible: isLoaded,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+          child: Container(
+            decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(15),
+                ),
+                color: Colors.grey),
+            child: Column(children: [
+              const SizedBox(
+                height: 20,
               ),
-              color: Colors.grey),
-          child: Column(children: [
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              height: 20,
-              color: Colors.grey[700],
-              child: const SizedBox.expand(
-                child: Center(
-                  child: Text("Reservas activas"),
+              Container(
+                height: 20,
+                color: Colors.grey[700],
+                child: const SizedBox.expand(
+                  child: Center(
+                    child: Text("Prestamos activos"),
+                  ),
                 ),
               ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(15),
-                    ),
-                    color: Colors.grey),
-                child: ListView.builder(
-                  itemCount: 2,
-                  primary: false,
-                  itemBuilder: (context, i) {
-                    return LibroCard(
-                      libro: Libro(),
-                      boton: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.red,
-                          ),
-                          onPressed: () {},
-                          child: const Text("Cancelar")),
-                    );
-                  },
+              Expanded(
+                flex: 1,
+                child: Container(
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
+                      ),
+                      color: Colors.grey),
+                  child: ListView.builder(
+                    itemCount: 2,
+                    primary: false,
+                    itemBuilder: (context, i) {
+                      return LibroCard(
+                        libro: Libro(),
+                        boton: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.red,
+                            ),
+                            onPressed: () {},
+                            child: const Text("Cancelar")),
+                      );
+                    },
+                  ),
                 ),
               ),
-            ),
-            Container(
-              height: 20,
-              color: Colors.grey[700],
-              child: const SizedBox.expand(
-                child: Center(
-                  child: Text("Historial de reservas"),
+              Container(
+                height: 20,
+                color: Colors.grey[700],
+                child: const SizedBox.expand(
+                  child: Center(
+                    child: Text("Prestamos"),
+                  ),
                 ),
               ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(15),
-                    ),
-                    color: Colors.grey),
-                child: ListView.builder(
-                  itemCount: 5,
-                  itemBuilder: (context, i) {
-                    return LibroCard(
-                      libro: Libro(),
-                    );
-                  },
+              Expanded(
+                flex: 2,
+                child: Container(
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
+                      ),
+                      color: Colors.grey),
+                  child: ListView.builder(
+                    itemCount: prestamos?.length,
+                    itemBuilder: (context, i) {
+                      return LibroCard(
+                        libro: Libro(),
+                      );
+                    },
+                  ),
                 ),
               ),
-            ),
-          ]),
+            ]),
+          ),
+        ),
+        replacement: const Center(
+          child: CircularProgressIndicator(),
         ),
       ),
     );
