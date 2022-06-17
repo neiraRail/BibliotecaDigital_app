128a36260c5e9959e4adca553dd4bb510865637c
Rail Neira <neira.rail@gmail.com>
2022-06-16 23:03:33 -0400

    Agrea dependencia para dropzone

--- lib/web/carga_libros.dart
+++ lib/web/carga_libros.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dropzone/flutter_dropzone.dart';
 
 class CargaLibros extends StatefulWidget {
   const CargaLibros({Key? key}) : super(key: key);
