5f016d88498b1bc02ee728c2bfe966a831ce5e92
Rail Neira <neira.rail@gmail.com>
2022-06-26 20:24:47 -0400

    Subida de archivo con libros implementada

--- lib/services/archivoService.dart
+++ lib/services/archivoService.dart
@@ -1,23 +1,40 @@
 import 'package:http/http.dart' as http;
-
-import '../modelo/file_DataModel.dart';
+import 'package:cross_file/cross_file.dart';
 
 class ArchivoService {
-  static void enviar(File_Data_Model? file) async {
+  static void enviar(XFile file) async {
     var client = http.Client();
 
-    var uri = Uri.http("localhost:7102", "/api/libro/", {'q': '{http}'});
+    print("Path: ");
+    print(file.readAsString());
+    var uri = Uri.http("localhost:5102", "/api/archivos/", {'q': '{http}'});
     var request = http.MultipartRequest('POST', uri);
-    var bytes = await file!.stream.single;
-    request.files.add(http.MultipartFile.fromBytes('file', bytes));
+    //request.files.add(http.MultipartFile.fromBytes('file', bytes));
+    request.files.add(await http.MultipartFile.fromPath('file', file.path));
     request.headers["Access-Control-Allow-Origin"] = "*";
     var res = await request.send();
 
     if (res.statusCode == 200) {
       print("Todo bien :)");
+      print(await res.stream.bytesToString());
     } else {
-      print("Todo maaal:C");
+      print("El servidor mandó un status code distinto de 200:C --->");
+      print(res.statusCode);
+      print(await res.stream.bytesToString());
     }
+
+    // var response = await client.post(uri, body: , headers: {
+    //   "Content-Type": "multipart/form-data",
+    //   "Accept": "application/json",
+    //   "Access-Control-Allow-Origin": "*"
+    // });
+    // if (response.statusCode == 200) {
+    //   var json = response.body;
+    //   print(json);
+    // } else {
+    //   print(response.body);
+    // }
+
     return null;
   }
 }
--- lib/web/carga_libros.dart
+++ lib/web/carga_libros.dart
@@ -1,10 +1,11 @@
 import 'package:bib_digitalapp/web/drop_zone_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dropzone/flutter_dropzone.dart';
-
+import 'package:desktop_drop/desktop_drop.dart';
 import '../modelo/file_DataModel.dart';
 import '../services/archivoService.dart';
 import 'dropped_file_widget.dart';
+import 'package:cross_file/cross_file.dart';
 
 class CargaLibros extends StatefulWidget {
   const CargaLibros({Key? key}) : super(key: key);
@@ -14,8 +15,7 @@ class CargaLibros extends StatefulWidget {
 }
 
 class _CargaLibrosState extends State<CargaLibros> {
-  late DropzoneViewController controller;
-  File_Data_Model? file;
+  late XFile _file;
 
   @override
   Widget build(BuildContext context) {
@@ -24,15 +24,20 @@ class _CargaLibrosState extends State<CargaLibros> {
         title: const Text("Carga de libros"),
       ),
       body: Column(children: [
-        DropZoneWidget(
-          onDroppedFile: (file) => setState(
-            () => {this.file = file},
-          ),
+        Wrap(
+          direction: Axis.horizontal,
+          runSpacing: 8,
+          spacing: 8,
+          children: [
+            ExmapleDragTarget(
+              onDropped: (file) {
+                setState(() {
+                  _file = file;
+                });
+              },
+            ),
+          ],
         ),
-        const SizedBox(
-          height: 10,
-        ),
-        DroppedFileWidget(file: file),
         Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
@@ -58,10 +63,10 @@ class _CargaLibrosState extends State<CargaLibros> {
               ),
               onPressed: () {
                 try {
-                  ArchivoService.enviar(file);
+                  ArchivoService.enviar(_file);
                 } catch (e) {
-                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                    content: Text("Hubo un error"),
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text(e.toString()),
                     backgroundColor: Colors.red,
                   ));
                 }
@@ -80,6 +85,81 @@ class _CargaLibrosState extends State<CargaLibros> {
           ],
         )
       ]),
+    );
+  }
+}
+
+class ExmapleDragTarget extends StatefulWidget {
+  const ExmapleDragTarget({Key? key, required this.onDropped})
+      : super(key: key);
+  final Function onDropped;
+
+  @override
+  _ExmapleDragTargetState createState() => _ExmapleDragTargetState();
+}
+
+class _ExmapleDragTargetState extends State<ExmapleDragTarget> {
+  List<XFile> _list = [];
+
+  bool _dragging = false;
+
+  Offset? offset;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropTarget(
+      onDragDone: (detail) async {
+        setState(() {
+          _list = [detail.files.last];
+          widget.onDropped(_list[0]);
+        });
+
+        debugPrint('onDragDone:');
+        for (final file in detail.files) {
+          debugPrint('  ${file.path} ${file.name}'
+              '  ${await file.lastModified()}'
+              '  ${await file.length()}'
+              '  ${file.mimeType}');
+        }
+      },
+      onDragUpdated: (details) {
+        setState(() {
+          offset = details.localPosition;
+        });
+      },
+      onDragEntered: (detail) {
+        setState(() {
+          _dragging = true;
+          offset = detail.localPosition;
+        });
+      },
+      onDragExited: (detail) {
+        setState(() {
+          _dragging = false;
+          offset = null;
+        });
+      },
+      child: Container(
+        height: 200,
+        width: 200,
+        color: _dragging ? Colors.blue.withOpacity(0.4) : Colors.black26,
+        child: Stack(
+          children: [
+            if (_list.isEmpty)
+              const Center(child: Text("Drop here"))
+            else
+              Text(_list.map((e) => e.path).join("\n")),
+            if (offset != null)
+              Align(
+                alignment: Alignment.topRight,
+                child: Text(
+                  '$offset',
+                  style: Theme.of(context).textTheme.caption,
+                ),
+              )
+          ],
+        ),
+      ),
     );
   }
 }
