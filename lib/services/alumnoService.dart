cb4848930fe1a8419d4d9ffd85a91199e919f0ba
michellenaour <54044891+michellenaour@users.noreply.github.com>
2022-07-07 23:12:44 -0400

    alumno perfil

--- lib/services/alumnoService.dart
+++ lib/services/alumnoService.dart
@@ -13,6 +13,20 @@ class AlumnoService {
     });
     if (response.statusCode == 200) {
       var json = response.body;
+      return alumnosFromJson(json);
+    }
+    return null;
+  }
+
+   static Future<Alumno?> getAlumno(int id) async {
+    var client = http.Client();
+    var uri = Uri.http("200.13.5.14:7102", "/api/alumno/"+id.toString(), {'q': '{http}'});
+    var response = await client.get(uri, headers: {
+      'Authorization': 'Bearer ' + GlobalData.token.toString(),
+      "Access-Control-Allow-Origin": "*"
+    });
+    if (response.statusCode == 200) {
+      var json = response.body;
       return alumnoFromJson(json);
     }
     return null;
